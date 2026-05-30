d2c594eaa5c9aa9bb38ab07f468f73f65b18babf
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-30 22:21:12 +0300

    update the button

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,4 +1,5 @@
 import { words } from "../constants";
+import Button from "../components/Button";
 
 function Hero() {
   return (
@@ -13,18 +14,21 @@ function Hero() {
           <div className="flex flex-col gap-7">
             <div className="hero-text">
               <h1>
-               
                 shaping
                 <span className="slide">
                   <span className="wrapper">
-                    {words.map((word) =>(
-                      <span key={word.text} className="flex items-center md:gap-3" gap-1 pb-2>
-                        <img 
-                        src={word.imgPath} 
-                        alt={word.text} 
-                        className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1
+                        pb-2"
+                        
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
                         />
-
                         <span>{word.text}</span>
                       </span>
                     ))}
@@ -34,10 +38,15 @@ function Hero() {
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
-             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">I'm Abrham, a full-stack developer based in Ethiopia, passionate about building modern web applications.</p>
-             <button className="md:w-80 md:h-16 w-60 h-12"
-             id="button"
-             text="See my Work"/>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              I'm Abrham, a full-stack developer based in Ethiopia, passionate
+              about building modern web applications.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my Work"
+            />
           </div>
         </header>
         {/* RIGHT: 3D MODEL */}
