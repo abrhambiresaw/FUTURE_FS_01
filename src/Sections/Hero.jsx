4f03635db5fd3a212f4820a9611dbcd9709d4088
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-30 21:29:15 +0300

    add button file

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -35,6 +35,9 @@ function Hero() {
               <h1>that Deliver Results</h1>
             </div>
              <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">I'm Abrham, a full-stack developer based in Ethiopia, passionate about building modern web applications.</p>
+             <button className="md:w-80 md:h-16 w-60 h-12"
+             id="button"
+             text="See my Work"/>
           </div>
         </header>
         {/* RIGHT: 3D MODEL */}
