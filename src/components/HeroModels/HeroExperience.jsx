dcefd99f6ee21cd93d196ad501fe2e24de246f87
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-31 14:52:03 +0300

    create file for 3d in hero section

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,8 +1,17 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 function HeroExperience() {
   return (
-    <div>HeroExperience</div>
+    <Canvas camera={{ position: [0, 0, 15], fov: 45}}>
+      <ambientLight intensity={0.2} color="#1a1a40"/>
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+    </Canvas>
   )
 }
 
