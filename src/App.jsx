d1d74b66ec183892a3cf3a0c634c3a47869044f0
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-06-02 14:50:53 +0300

    Implement GSAP scroll animations for showcase projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/Hero'
+import ShowcaseSection from './Sections/ShowcaseSection'
 
 function App() {
   return (
     <>
       <Hero />
+      <ShowcaseSection />
     </>
   )
 }
