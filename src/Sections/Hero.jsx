2ac8dbb61bcca6ae1c7546a53690d1d4ec24d3a0
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-06-02 10:32:12 +0300

    add animatedCounter file and also change the value inside counterItems

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -3,6 +3,8 @@ import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter";
+
 gsap.registerPlugin(useGSAP);
 
 function Hero() {
@@ -77,6 +79,8 @@ function Hero() {
           </div>
         </figure>
       </div>
+      
+      <AnimatedCounter />
     </section>
   );
 }
