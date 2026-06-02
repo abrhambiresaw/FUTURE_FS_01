fc2de93692872d69019ff24bed3d2cca775a1576
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-06-02 20:17:42 +0300

    create NavBar file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import ShowcaseSection from './Sections/ShowcaseSection'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
       <Hero />
       <ShowcaseSection />
+      <NavBar />
     </>
   )
 }
